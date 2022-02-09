63bbc0887e2b60c8a51f7d0eaa72b41198c98b18
BarbaH <barba-anto@live.it>
2022-02-09 21:34:47 +0100

    Added first functionalities

---
+++ tasks.py
@@ -0,0 +1,9 @@
+from discord.ext import tasks
+
+daily_tasks = {
+    669694249243115544: [{
+        'execution_time': '20:53',
+        'last_execution': '',
+        'text': 'everyone remember to claim ruins!'
+    }]
+}
